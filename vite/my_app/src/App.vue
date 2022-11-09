641bbfc99d647cd90cdf1195c591996b63ea12f2
karfaisarra <karfaisarra@gmail.com>
2022-11-09 15:04:01 +0100

    add component AppTitle

---
+++ vite/my_app/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+  <AppTitle></AppTitle>
+</template>
+
+<style scoped>
+
+</style>
